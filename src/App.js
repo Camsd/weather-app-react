9fb395f8530d82917527b11e4a58608b9c86e803
Camsd <codecos.info@gmail.com>
2023-08-01 12:09:46 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+
+// import axios
+import axios from "axios";
+
+//import icons
+import {
+  IoMdSunny,
+  IoMdRainy,
+  IoMdCloudy,
+  IoMdSnow,
+  IoMdThunderstorm,
+  IoMdSearch,
+} from "react-icons/io";
+
+import {BsCloudHaze2Fill, BsCloudDrizzleFill, BsEye, BsWater, BsThermometer, BsWind} from 'react-icons/bs'
 
 const App = () => {
   return <div>react app</div>;
